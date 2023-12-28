e99ecfdd79a7b5d55504da2db8e46a386e67a14e
seonkyoung <sunkyoung.dev@gmail.com>
2023-12-28 12:31:55 +0900

    setting4

---
+++ src/main/java/com/meatjellyburgur/musicpipe/MusicpipeApplication.java
@@ -0,0 +1,13 @@
+package com.meatjellyburgur.musicpipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicpipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicpipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meatjellyburgur/musicpipe/MusicpipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meatjellyburgur.musicpipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicpipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
