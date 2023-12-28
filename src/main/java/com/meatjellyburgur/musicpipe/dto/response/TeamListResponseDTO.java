5be9628335b37a791ec2cfe7d052b8be4e181c88
rudrbgus <rudrbgus3@gmail.com>
2023-12-28 16:55:25 +0900

    주석 제거

--- src/main/java/com/meatjellyburgur/musicpipe/dto/response/TeamListResponseDTO.java
+++ src/main/java/com/meatjellyburgur/musicpipe/dto/response/TeamListResponseDTO.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Builder
 public class TeamListResponseDTO {
     private int count;
-    //private PageMaker pageInfo;
+    private PageMaker pageInfo;
     private List<TeamDetailResponseDTO> teamDetailList;
 
 }
