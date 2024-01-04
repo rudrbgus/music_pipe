d7df43b265e6212f14ec643372fc600c406dc672
seonkyoung <sunkyoung.dev@gmail.com>
2024-01-04 22:05:15 +0900

    UserController -> /profile 부분 수정

--- src/main/java/com/meatjellyburgur/musicpipe/controller/UserController.java
+++ src/main/java/com/meatjellyburgur/musicpipe/controller/UserController.java
@@ -7,6 +7,7 @@ import com.meatjellyburgur.musicpipe.dto.request.SignInRequestDTO;
 import com.meatjellyburgur.musicpipe.dto.request.SignUpRequestDTO;
 import com.meatjellyburgur.musicpipe.dto.request.UserInstrumentRequestDTO;
 import com.meatjellyburgur.musicpipe.dto.response.FindUserResponseDTO;
+import com.meatjellyburgur.musicpipe.dto.response.SignInUserResponseDTO;
 import com.meatjellyburgur.musicpipe.dto.response.UserProfileResponseDTO;
 import com.meatjellyburgur.musicpipe.entity.User;
 import com.meatjellyburgur.musicpipe.service.InstrumentService;
@@ -184,8 +185,10 @@ public class UserController {
 
     // 유저 프로필 주는 url
     @GetMapping("/profile")
-    public String showProfile(String email, Model model) {
+    public String showProfile(HttpSession session, Model model) {
         log.debug("user/profile POST!!!");
+        SignInUserResponseDTO loginDTO =(SignInUserResponseDTO) session.getAttribute("login");
+        String email = loginDTO.getEmail();
         log.info("email: {}", email);
         User user = userService.getUser(email);
         UserProfileResponseDTO dto = UserProfileResponseDTO.builder()
